7cecf641d1c88ae8d322df04b277cf8f012ab10f
dom-inic <dominicnyambane22@gmail.com>
2023-12-03 22:45:43 +0300

    secret_key, static

--- studentexpensetracker/settings.py
+++ studentexpensetracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-1oyf-$(bb**1xd_i5+0^0na4sg2#ye8h+$*8arta$zekcz1+yz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
