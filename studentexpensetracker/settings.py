9ae2529bd446221c8dc530868700a08d54579e58
dom-inic <dominicnyambane22@gmail.com>
2023-12-04 16:43:40 +0300

    s3 storage

--- studentexpensetracker/settings.py
+++ studentexpensetracker/settings.py
@@ -153,20 +153,20 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
-# AWS_ACCESS_KEY_ID=os.environ['AWS_ACCESS_KEY_ID']
-# AWS_SECRET_ACCESS_KEY=os.environ['AWS_SECRET_ACCESS_KEY']
-# AWS_STORAGE_BUCKET_NAME=os.environ['AWS_STORAGE_BUCKET_NAME']
-# AWS_QUERYSTRING_AUTH = False
+AWS_ACCESS_KEY_ID=os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY=os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME=os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_QUERYSTRING_AUTH = False
 
 # s3 storages
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
 
 # enable this is using aws storage
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# AWS_HEADERS = {  
-#     # see http://developer.yahoo.com/performance/rules.html#expires
-#     'Expires': 'Sun, 27 Feb 2099 20:00:00 GMT',
-#     'Cache-Control': 'max-age=94608000',
-# }
+AWS_HEADERS = {  
+    # see http://developer.yahoo.com/performance/rules.html#expires
+    'Expires': 'Sun, 27 Feb 2099 20:00:00 GMT',
+    'Cache-Control': 'max-age=94608000',
+}
