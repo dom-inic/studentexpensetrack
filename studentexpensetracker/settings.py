106c7f986841ba06a0e15f125b1438869ae474ce
dom-inic <dominicnyambane22@gmail.com>
2023-12-04 13:32:22 +0300

    static files

--- studentexpensetracker/settings.py
+++ studentexpensetracker/settings.py
@@ -138,10 +138,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -164,8 +164,8 @@ REST_FRAMEWORK = {
 # enable this is using aws storage
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_HEADERS = {  
-    # see http://developer.yahoo.com/performance/rules.html#expires
-    'Expires': 'Sun, 27 Feb 2099 20:00:00 GMT',
-    'Cache-Control': 'max-age=94608000',
-}
+# AWS_HEADERS = {  
+#     # see http://developer.yahoo.com/performance/rules.html#expires
+#     'Expires': 'Sun, 27 Feb 2099 20:00:00 GMT',
+#     'Cache-Control': 'max-age=94608000',
+# }
